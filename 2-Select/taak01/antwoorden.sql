9b7ab8720b519b01d0af77a0cfb3705207b0b837
jochem <jochem.gravendeel@student.rocva.nl>
2020-05-26 10:48:34 +0200

    taak 2 af

--- 2-Select/taak01/antwoorden.sql
+++ 2-Select/taak01/antwoorden.sql
@@ -1,13 +1,13 @@
 -- Opdracht 1 - voorbeeld
 SELECT * FROM jaar2016
 -- Opdracht 2 
-
+SELECT * from jaar2015
 -- Opdracht 3
-
+SELECT * FROM `jaar2016` WHERE 1
 -- Opdracht 4
-
+SELECT region from jaar2015
 -- Opdracht 5
-
+SELECT region, score FROM jaar2015
 -- Opdracht 6
-
+SELECT rank, score from jaar2016
 -- Opdracht 7
