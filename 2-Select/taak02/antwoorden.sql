35ad4eec380f7b1b3e87173cb6cd7d85facf748e
jochem <jochem.gravendeel@student.rocva.nl>
2020-05-26 11:01:59 +0200

    af

--- 2-Select/taak02/antwoorden.sql
+++ 2-Select/taak02/antwoorden.sql
@@ -1,7 +1,7 @@
 -- Opdracht 1 
-
+SELECT country FROM jaar2015 WHERE rank = 7
 -- Opdracht 2 
-
+SELECT rank FROM jaar2016 where country = Nederland
 -- Opdracht 3
 
 -- Opdracht 4
@@ -17,3 +17,4 @@
 -- Opdracht 9
 
 -- Opdracht 10
+SELECT country FROM jaar2016 WHERE region = Latin America and Caribbean
