71205e1fedf725801152f55e56de4d837220d22c
jochem <jochem.gravendeel@student.rocva.nl>
2020-05-27 11:43:44 +0200

    taak 4 af

--- 2-Select/taak04/antwoorden.sql
+++ 2-Select/taak04/antwoorden.sql
@@ -1,19 +1,19 @@
 -- Opdracht 1 
-
+SELECT Max(wage) as hoogste_loon_speler_fc_utrecht from players WHERE club = "FC Utrecht"
 -- Opdracht 2 
-
+SELECT AVG(wage) as AVG_wage from players
 -- Opdracht 3
-
+SELECT SUM(wage) as som FROM players WHERE club = "FC Groningen"
 -- Opdracht 4
 
 -- Opdracht 5
-
+SELECT AVG(wage) FROM players WHERE nationality = "Netherlands"
 -- Opdracht 6 
-
+SELECT AVG(wage) as gemiddelde_20_salaris FROM players WHERE age <= 20
 -- Opdracht 7 
-
+SELECT AVG(wage) as gemiddelde_20_salaris FROM players WHERE age >= 20
 -- Opdracht 8
-
+SELECT sum(wage) as speler_waarde FROM players where club = "Chelsea"
 -- Opdracht 9
 
 -- Opdracht 10
